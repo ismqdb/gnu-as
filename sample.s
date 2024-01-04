3f13f370de602c5ec47f6f99a759588039803eb3
Milos Djukic <djukic.djukic96@gmail.com>
2024-01-04 18:55:31 +0100

    add shit

--- sample.s
+++ sample.s
@@ -2,6 +2,19 @@
 .section .text
 
 _start:
+    movb $0xFF, %al
+    addb $0xFF, %ah
+
+    movw $0xFF00, %ax
+    addw $0x00FF, %ax
+
+    movl $0xFFFF0000, %eax
+    addl $0x0000FFFF, %eax
+
+    movabsq $0xFFFFFFFF00000000, %rax
+    movabsq $0x00000000FFFFFFFF, %rbx
+
+    addq %rbx, %rax
+
     movq $60, %rax
-    movq $3, %rdi
-    //syscall
+    syscall
