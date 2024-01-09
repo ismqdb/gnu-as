d313826545d1377f9f1a7caa49805998c4c8d152
Milos Djukic <djukic.djukic96@gmail.com>
2024-01-09 15:23:13 +0100

    add shit

--- Snippets/21. persondatapointer.s
+++ Snippets/21. persondatapointer.s
@@ -4,27 +4,34 @@
         .quad (endpeople-people)/PERSON_RECORD_SIZE
     people:
         # Array of people
-        .ascii "Gilbert Keith Chester\0"
-        .quad 200, 10, 2, 74, 20
-        .ascii "Jonathan Bartlett\0"
-        .quad 280, 12, 2, 72, 44 # me!
-        .ascii "Clive Silver Lewis\0"
-        .quad 150, 8, 1, 68, 30
-        .ascii "Tommy Aquinas\0"
-        .quad 250, 14, 3, 75, 24
-        .ascii "Isaac Newton\0"
-        .quad 250, 10, 4, 70, 11
-        .ascii "Gregory Mend\0"
-        .quad 180, 11, 5, 69, 65
+        .quad $gkcname, 200, 10, 2, 74, 20
+        .quad $jbname, 280, 12, 2, 72, 44 # me!
+        .quad $cslname, 150, 8, 1, 68, 30
+        .quad $taname, 250, 14, 3, 75, 24
+        .quad $inname, 250, 10, 4, 70, 11
+        .quad $gmname, 180, 11, 5, 69, 65
     endpeople:
 
-    .globl NAME_OFFSET
+    gkcname:
+        .ascii "Gilbert Keith Chester\0"
+    jbname:
+        .ascii "Jonathan Bartlett\0"
+    cslname:
+        .ascii "Clive Silver Lewis\0"
+    taname: 
+        .ascii "Tommy Aquinas\0"
+    inname:
+        .ascii "Isaac Newton\0"
+    gmname:
+        .ascii "Gregory Mend\0"
+
+    .globl NAME_PTR_OFFSET
     .globl WEIGHT_OFFSET, HAIR_OFFSET
     .globl HEIGHT_OFFSET, AGE_OFFSET
     .globl SHOE_OFFSET
 
-    .equ NAME_OFFSET, 0
-    .equ WEIGHT_OFFSET, NAME_OFFSET+32
+    .equ NAME_PTR_OFFSET, 0
+    .equ WEIGHT_OFFSET, NAME_OFFSET+8
     .equ SHOE_OFFSET, WEIGHT_OFFSET+8
     .equ HAIR_OFFSET, SHOE_OFFSET+8
     .equ HEIGHT_OFFSET, HAIR_OFFSET+8
