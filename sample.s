e84504f1488c7ffb1379fc32412e335397c6aed6
Milos Djukic <djukic.djukic96@gmail.com>
2024-01-11 21:38:10 +0100

    add shit

--- sample.s
+++ sample.s
@@ -6,18 +6,20 @@
         movq $people, %rbx
         movq $0, %rdx
 
-        movq $numpeople, %rcx
+        movq $0, %rcx        
 
         nextperson:
             leaq AGE_OFFSET(%rbx), %rax
-            cmpq (%rax), %rdx
-            ja endloop
+            cmpq (%rax), %rdi
+            jae endloop
 
-            movb (%rax), %dl
+            movq (%rax), %rdi
 
             endloop:
                 addq $PERSON_RECORD_SIZE, %rbx
-                loopq nextperson
+                incq %rcx
+                cmpq %rcx, numpeople
+                jnz nextperson
         
         finish:
             movq $60, %rax
