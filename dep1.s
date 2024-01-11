cbaf3dcc6ab30f5271ab1356b319c998295cee77
Milos Djukic <djukic.djukic96@gmail.com>
2024-01-11 19:42:45 +0100

    add shit

--- dep1.s
+++ dep1.s
@@ -3,32 +3,20 @@
     numpeople:
         .quad (endpeople-people)/PERSON_RECORD_SIZE
     people:
-        # Array of people
-        .ascii "Gilbert Keith Chester\0"
-        .quad 200, 10, 2, 74, 20
-        .ascii "Jonathan Bartlett\0"
-        .quad 280, 12, 2, 72, 44 # me!
-        .ascii "Clive Silver Lewis\0"
-        .quad 150, 8, 1, 68, 30
-        .ascii "Tommy Aquinas\0"
-        .quad 250, 14, 3, 75, 24
-        .ascii "Isaac Newton\0"
-        .quad 250, 10, 4, 70, 11
-        .ascii "Gregory Mend\0"
-        .quad 180, 11, 5, 69, 65
+        .quad 200, 2, 74, 20
+        .quad 280, 2, 72, 44 # me!
+        .quad 150, 1, 68, 30
+        .quad 250, 3, 75, 24
+        .quad 250, 2, 70, 11
+        .quad 180, 5, 69, 65 
     endpeople:
 
-    .globl NAME_OFFSET
     .globl WEIGHT_OFFSET, HAIR_OFFSET
     .globl HEIGHT_OFFSET, AGE_OFFSET
-    .globl SHOE_OFFSET
-
-    .equ NAME_OFFSET, 0
-    .equ WEIGHT_OFFSET, NAME_OFFSET+32
-    .equ SHOE_OFFSET, WEIGHT_OFFSET+8
-    .equ HAIR_OFFSET, SHOE_OFFSET+8
-    .equ HEIGHT_OFFSET, HAIR_OFFSET+8
-    .equ AGE_OFFSET, HEIGHT_OFFSET+8
+    .equ WEIGHT_OFFSET, 0
+    .equ HAIR_OFFSET, 8
+    .equ HEIGHT_OFFSET, 16
+    .equ AGE_OFFSET, 24
 
     .globl PERSON_RECORD_SIZE
-    .equ PERSON_RECORD_SIZE, AGE_OFFSET+8
+    .equ PERSON_RECORD_SIZE, 32
--- sample.s
+++ sample.s
@@ -6,16 +6,18 @@
         movq $people, %rbx
         movq $0, %rdx
 
+        movq $numpeople, %rcx
+
         nextperson:
             leaq AGE_OFFSET(%rbx), %rax
             cmpq (%rax), %rdx
             ja endloop
 
-            movq (%rax), %rdi
+            movb (%rax), %dl
 
             endloop:
                 addq $PERSON_RECORD_SIZE, %rbx
-                jmp nextperson
+                loopq nextperson
         
         finish:
             movq $60, %rax
