85a593c385ad7ee95ae1aa68608e75b8f4bf3f5c
Milos Djukic <djukic.djukic96@gmail.com>
2024-01-15 20:39:53 -0100

    yes

--- sample.s
+++ sample.s
@@ -9,39 +9,7 @@
         # Name index
         movq $0, %rcx
 
-        # Pointer to name
-        movq $people, %rbx
-
-        checkname:
-            movq %rbx, %rax
-            addq %rcx, %rax
-
-            cmpq $0, (%rax)
-            je nextname
-
-            
-
-            incq %rcx
-            jmp checkname
-
-        nextname:
-            addq $PERSON_RECORD_SIZE, %rbx
-            movq $0, %rcx
-            cmpq %rcx, %rdi
-            jg newbiggest
-
-        newbiggest:
-            movq %rcx, %rdi
-            jmp nextname
-
-        isletter:
-            movq $0, %rsi
-
-        isLowercaseLetter:
-            
-
-        isUppercaseLetter:
-
+        
         
         finish:
             movq $60, %rax
