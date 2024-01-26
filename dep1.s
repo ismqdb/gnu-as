90665ec9b017c2454fe3b533c1088d08a422ff05
Milos Djukic <djukic.djukic96@gmail.com>
2024-01-26 21:20:10 +0100

    y

--- dep1.s
+++ dep1.s
@@ -1,34 +1,10 @@
 .section .data
-    .globl people, numpeople
-    numpeople:
-        .quad (endpeople-people)/PERSON_RECORD_SIZE
-    people:
-        # Array of people
-        .ascii "Gilbert Keith Chester\0"
-        .quad 200, 10, 2, 74, 20
-        .ascii "Jonathan Bartlett\0"
-        .quad 280, 12, 2, 72, 44 # me!
-        .ascii "Clive Silver Lewis\0"
-        .quad 150, 8, 1, 68, 30
-        .ascii "Tommy Aquinas\0"
-        .quad 250, 14, 3, 75, 24
-        .ascii "Isaac Newton\0"
-        .quad 250, 10, 4, 70, 11
-        .ascii "Gregory Mend\0"
-        .quad 180, 11, 5, 69, 65
-    endpeople:
+    .globl num1, num2, result
 
-    .globl NAME_OFFSET
-    .globl WEIGHT_OFFSET, HAIR_OFFSET
-    .globl HEIGHT_OFFSET, AGE_OFFSET
-    .globl SHOE_OFFSET
-
-    .equ NAME_OFFSET, 0
-    .equ WEIGHT_OFFSET, 32
-    .equ SHOE_OFFSET, 40
-    .equ HAIR_OFFSET, 48
-    .equ HEIGHT_OFFSET, 56
-    .equ AGE_OFFSET, 64
-
-    .globl PERSON_RECORD_SIZE
-    .equ PERSON_RECORD_SIZE, 72
+    num1:
+        .quad 7, 8, 1, 2, 4
+    num2:
+        .quad 1, 3, 4, 4, 9
+    result:
+        .quad 0, 0, 0, 0, 0
+    
