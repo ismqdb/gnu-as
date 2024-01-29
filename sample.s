f43c30b31f131797a2386222bd874188d60925de
Milos Djukic <djukic.djukic96@gmail.com>
2024-01-29 19:40:09 +0100

    u

--- sample.s
+++ sample.s
@@ -1,13 +1,52 @@
-.globl _start, programEnd
+.globl main
 .section .data
-    num:
-        .quad 4
+    filename:
+        .ascii "myfile.txt\0"
+    openmode:
+        .ascii "w\0"
+    formatstring1:
+        .ascii "The age of %s is %d.\n\0"
+    sallyname:
+        .ascii "Sally\0"
+    sallyage:
+        .quad 53
+    formatstring2:
+        .ascii "%d and %d are %s's favorite numbers.\n\0"
+    joshname:
+        .ascii "Josh\0"
+    joshfavoritefirst:
+        .quad 7
+    joshfavoritesecond:
+        .quad 13
+        
 
 .section .text
-    _start:
-        movq num, %rax
-        movq num, %rdi
-        call factOrExp
+    main:
+        enter $16, $0
+
+        movq $filename, %rdi
+        movq $openmode, %rsi
+        call fopen
+
+        movq %rax, -8(%rbp)
+
+        movq -8(%rbp), %rdi
+        movq $formatstring1, %rsi
+        movq $sallyname, %rdx
+        movq sallyage, %rcx
+        movq $0, %rax
+        call fprintf
+
+        movq -8(%rbp), %rdi
+        movq $formatstring2, %rsi
+        movq joshfavoritefirst, %rdx
+        movq joshfavoritesecond, %rcx
+        movq $joshname, %r8
+        movq $0, %rax
+        call fprintf
+
+        movq -8(%rbp), %rdi
+        call fclose
         
     programEnd:
         movq $60, %rax
