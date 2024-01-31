75cfc947df20e48606445d3b78c9c7e1f424c9d3
Milos Djukic <djukic.djukic96@gmail.com>
2024-01-31 19:53:45 +0100

    y

--- dep1.s
+++ dep1.s
@@ -1,20 +1,42 @@
-.globl exponent
-.type exponent, @function
+.globl main
+.section .data
+    promptformat:
+        .ascii "Enter two numbers separated by space, then press return.\n\0"
+
+    scanformat:
+        .ascii "%d %d\0"
+
+    resultformat:
+        .ascii "The result is %d.\n\0"
 
 .section .text
+.equ LOCAL_NUMBER, -8
+.equ LOCAL_EXPONENT, -16
 
-exponent:
+main__:
     enter $16, $0
 
-    movq $1, %rax
+    movq stdout, %rdi
+    movq $promptformat, %rsi
+    movq $0, %rax
+    call fprintf
 
-    movq %rsi, -8(%rbp)
+    movq stdin, %rdi
+    movq $scanformat, %rsi
+    leaq LOCAL_NUMBER(%rbp), %rdx
+    leaq LOCAL_EXPONENT(%rbp), %rcx
+    movq $0, %rax
+    call fscanf
 
-mainloop:
-    mulq %rdi
-    decq -8(%rbp)
-    jnz mainloop
+    movq LOCAL_NUMBER(%rbp), %rdi
+    movq LOCAL_EXPONENT(%rbp), %rsi
+    //call exponent
 
-complete:
+    movq stdout, %rdi
+    movq $resultformat, %rsi
+    movq %rax, %rdx
+    movq $0, %rax
+    call fprintf
+
     leave
     ret
--- sample.s
+++ sample.s
@@ -1,42 +1,115 @@
 .globl main
 .section .data
-    promptformat:
-        .ascii "Enter two numbers separated by space, then press return.\n\0"
+    prompt:
+        .ascii "Enter a number:\0"
+    inputStr:
+        .ascii "%d\0"
+    numStr:
+        .ascii "Number %d is %s.\n\0"
+    oddStr:
+        .ascii "odd.\n\0"
+    evenStr:
+        .ascii "even.\n\0"
+    num:
+        .quad 0
 
-    scanformat:
-        .ascii "%d %d\0"
+    main:
+        enter $16, $0
 
-    resultformat:
-        .ascii "The result is %d.\n\0"
+        movq stdout, %rdi
+        movq $prompt, %rsi
+        movq $0, %rax
+        call fprintf
 
-.section .text
-.equ LOCAL_NUMBER, -8
-.equ LOCAL_EXPONENT, -16
+        movq stdin, %rdi
+        movq $inputStr, %rsi
+        movq $num, %rdx
+        movq $0, %rax
+        call fscanf
 
-main:
-    enter $16, $0
+        movq $2, %rbx
+        divq %rbx
+        cmpq $0, %rdx
+        jz even
+        jnz odd
+        
+    finish:
+        movq %rax, %rdi
+        movq $60, %rax
+        syscall
 
-    movq stdout, %rdi
-    movq $promptformat, %rsi
-    movq $0, %rax
-    call fprintf
+    printEvenString:
+        movq stdout, %rdi
+        movq $numStr, %rsi
+        movq num, %rdx
+        movq $evenStr, %rcx
+        call fprintf
 
-    movq stdin, %rdi
-    movq $scanformat, %rsi
-    leaq LOCAL_NUMBER(%rbp), %rdx
-    leaq LOCAL_EXPONENT(%rbp), %rcx
-    movq $0, %rax
-    call fscanf
+        ret
 
-    movq LOCAL_NUMBER(%rbp), %rdi
-    movq LOCAL_EXPONENT(%rbp), %rsi
-    call exponent
+    printOddString:
+        movq stdout, %rdi
+        movq $numStr, %rsi
+        movq num, %rdx
+        movq $oddStr, %rcx
+        call fprintf
 
-    movq stdout, %rdi
-    movq $resultformat, %rsi
-    movq %rax, %rdx
-    movq $0, %rax
-    call fprintf
+        ret
 
-    leave
-    ret
+    odd:
+        pushq %rbp
+        movq %rsp, %rbp
+        subq $16, %rsp
+
+        pushq %rax
+        pushq %rdi
+        pushq %rcx
+        pushq %rsi
+        pushq %rdx
+
+        movq $1, %rax
+        movq $1, %rdi
+
+        movq $10, %rcx
+
+        call printOddString
+
+        popq %rdx
+        popq %rsi
+        popq %rcx
+        popq %rdi
+        #popq %rax
+
+        movq %rbp, %rsp
+        popq %rbp
+
+        jmp finish
+
+    even:
+        pushq %rbp
+        movq %rsp, %rbp
+        subq $16, %rsp
+
+        pushq %rax
+        pushq %rdi
+        pushq %rcx
+        pushq %rsi
+        pushq %rdx
+
+        movq $1, %rax
+        movq $1, %rdi
+
+        movq $10, %rcx
+
+        call printEvenString
+
+        popq %rdx
+        popq %rsi
+        popq %rcx
+        popq %rdi
+        #popq %rax
+
+        movq %rbp, %rsp
+        popq %rbp
+
+        jmp finish
